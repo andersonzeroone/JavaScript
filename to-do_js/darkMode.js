f5a44e8d7a624c86adab82e8ad6cf570c542dbb4
P <anderson.pablo02@gmail.ccom>
2020-04-25 00:01:00 -0300

    up

--- to-do_js/darkMode.js
+++ to-do_js/darkMode.js
@@ -1,5 +1,12 @@
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+ if (ev.target.tagName === 'LI') {
+     ev.target.classList.toggle('checked');
+    }
+}, false);
+
 const html = document.querySelector("html");
-const checkbox = document.querySelector("input[name=theme]");
+const checkbox = document.querySelector("#switch");
 
 const getStyle = (element, style) => 
     window
@@ -10,18 +17,21 @@ const initialColor = {
     bgg: getStyle(html, "--bgg"),
     bgTitle: getStyle(html, "--bg-title"),
     bgBnt: getStyle(html, "--bg-bnt"),
-    bgLi: getStyle(html, "--bg-li"),
+    bgBntcheck: getStyle(html, "--bg-bntcheck")
+    
 }
 
 const darkMode = {
-    bg: '#1d1c1c',
+    bgg: '#1d1c1c',
     bgTitle: '#EBEBEB',
     bgBnt:'#1d1c1c',    
-    bgLi: '#cac2c2'
+    bgBntcheck:'#3664FF'
+
+
 }
 
 const transformKey = key => 
-    "--" + key.replace(/([A-Z]))/, "-$1").toLowerCase()
+    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
 
 const changeColors = (colors) => {
     Object.keys(colors).map( key =>
