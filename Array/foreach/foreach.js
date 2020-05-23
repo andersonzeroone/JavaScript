2273ab946c6dcc3062d6ce9aace514ddb37c5d92
Anderson Pablo <anderson.pablo02@gmail.com>
2020-05-22 22:51:08 -0300

    Update foreach.js

--- Array/foreach/foreach.js
+++ Array/foreach/foreach.js
@@ -1,6 +1,6 @@
 const aprovados= ['Agata', 'Paula', 'maria', 'Pedro']
 
-aprovados.forEach( function(valor, indice){ // po primeiro param é o valor e o segundo os indeces do array
+aprovados.forEach( function(valor, indice){ // o primeiro param é o valor e o segundo os indeces do array
     console.log(` indice: ${indice + 1}  valor: ${valor}`)//executa a função para cada elemento do array
 });
 
