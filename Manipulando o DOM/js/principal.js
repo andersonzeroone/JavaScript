604a066efe2f69b7b3e4dcef3b4c0582b69243aa
andersonzeroone <anderson.pablo02@gmail.com>
2020-05-29 22:29:28 -0300

    manipulando o DOM

---
+++ Manipulando o DOM/js/principal.js
@@ -0,0 +1,42 @@
+// export default function velidacao(){
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for( var i =0; i< pacientes.length; i++){
+   
+    var paciente = pacientes[i];
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if (peso <= 0 || peso >= 1000) {
+        console.log("Peso inválido");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if (altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdImc.textContent = "Altura Inválido!";
+        paciente.classList.add("paciente-invalido");
+    }
+
+    if( pesoEhValido && alturaEhValida ){
+        var imc = peso /( altura * altura);
+        console.log(imc);
+        tdImc.textContent = imc.toFixed(2);
+    }
+  }
+// 
